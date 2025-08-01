fa080b510feca6644d1896088eaa7db032b1fbe8
Christopher Trent <ChristopherJTrent@outlook.com>
2025-08-01 16:14:38 -0700

    desperately trying random things

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,11 @@ import { Redirect } from 'expo-router';
 import { ActivityIndicator, Text, View } from 'react-native';
 
 export default function Root() {
-	return <>
+	return <View>
 		<View style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
 			<ActivityIndicator size="large" />
 			<Text>Loading... Please Wait.</Text>
 		</View>
 		<Redirect href="/(tabs)/home"/>
-	</>
+	</View>
 }
