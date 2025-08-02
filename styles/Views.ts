ffb40ede92e6142e170318e4260089b259ebb4dc
Christopher Trent <ChristopherJTrent@outlook.com>
2025-08-02 01:16:51 -0700

    add additional styles

--- styles/Views.ts
+++ styles/Views.ts
@@ -1,23 +1,45 @@
 import { StyleProp, ViewStyle } from 'react-native';
 
-type ViewStyles = {
-	centeredFlexColumn: StyleProp<ViewStyle>
-	centeredFlexRow: StyleProp<ViewStyle>
+type ViewStylesType = {
+	flexColumn: StyleProp<ViewStyle>
+	flexRow: StyleProp<ViewStyle>
+	justifyCenter: StyleProp<ViewStyle>
+	alignCenter: StyleProp<ViewStyle>
+	pad: (n:number) => StyleProp<ViewStyle>
+	padBottom: (n:number) => StyleProp<ViewStyle>
+	padLeft: (n:number) => StyleProp<ViewStyle>
+	padRight: (n:number) => StyleProp<ViewStyle>
+	padTop: (n:number) => StyleProp<ViewStyle>
+	margin: (n:number) => StyleProp<ViewStyle>
+	marginBottom: (n:number) => StyleProp<ViewStyle>
+	marginLeft: (n:number) => StyleProp<ViewStyle>
+	marginRight: (n:number) => StyleProp<ViewStyle>
+	marginTop: (n:number) => StyleProp<ViewStyle>
 }
 
-export const ViewStyleProps: ViewStyles = {
-	centeredFlexColumn: {
-		padding: 4,
+export const ViewStyles: ViewStylesType = {
+	flexColumn: {
 		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
+		flexDirection: 'column'
+	},
+	flexRow: {
+		display: 'flex',
+		flexDirection: 'row'
+	},
+	justifyCenter: {
+		justifyContent: 'center'
+	},
+	alignCenter: {
 		alignItems: 'center'
 	},
-	centeredFlexRow: {
-		padding: 4,
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
+	pad: (n) => ({padding: n}),
+	padBottom: (n) => ({paddingBottom: n}),
+	padLeft: (n) => ({paddingLeft: n}),
+	padRight: (n) => ({paddingRight: n}),
+	padTop: (n) => ({paddingTop: n}),
+	margin: (n) => ({margin: n}),
+	marginTop: (n) => ({marginTop: n}),
+	marginRight: (n) => ({marginRight: n}),
+	marginBottom: (n) => ({marginBottom: n}),
+	marginLeft: (n) => ({marginLeft:n})
 }
