88cd313971ed96254c46c02499a4af57db4bcef6
Christopher Trent <ChristopherJTrent@outlook.com>
2025-08-02 16:43:45 -0700

    add ability scores to character store

--- app/(tabs)/character.tsx
+++ app/(tabs)/character.tsx
@@ -1,9 +1,13 @@
+import { AbilityScores } from '@/components/AbilityScores';
 import ArmorClass from '@/components/ArmorClass';
 import { ThemedText } from '@/components/ThemedText';
+import { mergeStyles } from '@/styles/util';
+import { ViewStyles } from '@/styles/Views';
 import { Text, View } from 'react-native';
 
 export default function CharacterScreen() {
-	return <View>
+	return <View style = {mergeStyles(ViewStyles.flexRow)}>
+		<AbilityScores />
 		<ArmorClass />
 	</View>
 }
