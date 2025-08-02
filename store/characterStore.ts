36ecef3bafc24184ab1a27bd6e3739b8885be73d
Christopher Trent <ChristopherJTrent@outlook.com>
2025-08-02 16:45:09 -0700

    add ability scores to store

--- store/characterStore.ts
+++ store/characterStore.ts
@@ -1,13 +1,17 @@
 import { createDefaultPersistentStore } from './createPersistentStore'
 
+export type AbilityScoreName = "STR"|"DEX"|"CON"|"INT"|"WIS"|"CHA"
+
 interface CharacterState {
-	AC: number,
+	AC: number
+	AbilityScores: {[k in AbilityScoreName]: number}
 	setAC: (n: number) => void
+	setAbilityScore: (k: AbilityScoreName, v: number) => void
 }
 
 export const useCharacterStore = createDefaultPersistentStore<CharacterState>((set) => ({
 	AC: 10,
-	setAC: (n) => set(() => ({
-		AC: n
-	}))
+	AbilityScores: {STR: 10, DEX: 10, CON: 10, INT: 10, WIS: 10, CHA: 10},
+	setAC: (n) => set(() => ({AC: n})),
+	setAbilityScore: (k, v) => set((state) => ({AbilityScores: {...state.AbilityScores, [k]: v}}))
 }), 'character')
