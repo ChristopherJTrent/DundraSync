458aa0ff536ddd4b968ce9188c203f1049d969c8
Christopher Trent <ChristopherJTrent@outlook.com>
2025-08-02 01:17:02 -0700

    improve AC component

--- components/ArmorClass.tsx
+++ components/ArmorClass.tsx
@@ -1,24 +1,98 @@
 import { ShieldIcon } from '@/assets/vector/ShieldIcon';
 import { useCharacterStore } from '@/store/characterStore';
-import { ViewStyleProps } from '@/styles/Views';
-import { Button, Text, View } from 'react-native';
+import { mergeStyles } from '@/styles/util';
+import { ViewStyles } from '@/styles/Views';
+import Lucide from '@react-native-vector-icons/lucide';
+import { useState } from 'react';
+import { Button, Text, View, ViewStyle, StyleProp, TouchableHighlight, Modal, TextInput} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 
 
 export default function ArmorClass() {
 	const {AC, setAC} = useCharacterStore()
+	const [modalOpen, setModalOpen] = useState(false)
+	const [number, setNumber] = useState('')
 	return (
-	<View style = {ViewStyleProps.centeredFlexColumn}>
+	<View style = {mergeStyles(
+		ViewStyles.flexColumn, 
+		ViewStyles.justifyCenter, 
+		ViewStyles.alignCenter
+	)}>
+
+		<Modal
+			animationType='fade'
+			transparent={true}
+
+			visible={modalOpen}
+			onRequestClose={() => setModalOpen(false)}
+		>
+				<View style={mergeStyles(
+					ViewStyles.flexColumn,
+					ViewStyles.alignCenter,
+					ViewStyles.justifyCenter,
+					{
+						position: 'fixed',
+						top: 0,
+						left: 0,
+						width: '100%',
+						height: '100%',
+						backgroundColor: '#000000A0'
+					})}>
+				<View style={mergeStyles(
+					ViewStyles.flexColumn, 
+					ViewStyles.justifyCenter, 
+					ViewStyles.alignCenter,
+					ViewStyles.pad(16),
+					{
+						backgroundColor: 'white',
+						width: '40%'
+					})
+				}>
+					<TouchableHighlight style={{position:'absolute', top: 4, right: 4}} onPress={() => {setModalOpen(false)}}>
+						<Lucide name='x' size={32}></Lucide>
+					</TouchableHighlight>
+					<Text>Editing AC</Text>
+					<TextInput 
+						keyboardType='decimal-pad' 
+						value={number.toString()}
+						onSubmitEditing={() => {
+							if (!isNaN(Number(number))) {
+								setAC(Number(number))}
+								setModalOpen(false)
+						}}
+						onChangeText={setNumber}
+						style={{
+							borderColor:'black',
+							borderWidth: 2,
+							color: 'black',
+							width: 128
+						}}
+					></TextInput>
+				</View>
+			</View>
+		</Modal>
 		<Text style={{
 			position: 'absolute', 
 			color: "#000", 
 			fontSize: 32, 
-			zIndex: 1,
+			zIndex: -1,
 			top: 18
 		}}>{AC}</Text>
-		<ShieldIcon />
-		<View style={ViewStyleProps.centeredFlexRow}>
+		<TouchableHighlight 
+			hitSlop={16} 
+			onLongPress={() => {setModalOpen(true)}}
+			underlayColor={'#000000AA'}>
+			<ShieldIcon/>
+		</TouchableHighlight>
+		<View style={ mergeStyles(
+			ViewStyles.flexRow,
+			ViewStyles.alignCenter,
+			ViewStyles.justifyCenter,
+			ViewStyles.marginTop(6),
+		)}>
 			<Button title='+1' onPress={() => setAC(AC + 1)}/>
+			<View style={{width: 6}} />
 			<Button title='-1' onPress={() => setAC(AC - 1)}/>
 		</View>
 	</View>
